994e32e499d592035d534c46d24dec8d1cd66d70
itdice <itdiceofficial@gmail.com>
2023-06-09 16:36:31 +0900

    ncf 공백 제거

--- nfc.py
+++ nfc.py
@@ -7,7 +7,8 @@
 import subprocess
 
 with subprocess.Popen(["nfc-poll"], stdout=subprocess.PIPE) as process:
-    nfc_raw_data: str = process.stdout.read().decode("utf-8")
-    nfc_uid = nfc_raw_data[nfc_raw_data.find('UID')+14:nfc_raw_data.find('UID')+28]
+    nfc_raw_data: str = process.stdout.read().decode("utf-8")  # Read NFC Data
+    nfc_uid = nfc_raw_data[nfc_raw_data.find('UID')+14:nfc_raw_data.find('UID')+28]  # Get UID Data
+    nfc_uid.replace("  ", "")  # Remove Space
 
 print(f"Result => {nfc_uid}")
