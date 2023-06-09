0205a284b6305b109cd64601dc3bf7591656cbba
itdice <itdiceofficial@gmail.com>
2023-06-09 18:00:44 +0900

    Ultrasonic 추가

--- ultrasonic.py
+++ ultrasonic.py
@@ -4,3 +4,39 @@
 #
 # Created by IT DICE on 2023/06/05.
 #
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+TRIG = 23
+ECHO = 24
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.output(TRIG, False)
+print("Ultrasonic Initialize")
+time.sleep(2)
+
+try:
+    while True:
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+            start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+            stop = time.time()
+
+        check_time = stop - start
+        distance = check_time * 34300 / 2
+        print("Distance : %.1f cm" % distance)
+        time.sleep(0.4)
+
+except KeyboardInterrupt:
+    print("Exit")
+    GPIO.cleanup()
