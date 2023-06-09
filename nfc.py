f8c988994f75bddc8d969a9c4f22f6d34fee1c3c
itdice <itdiceofficial@gmail.com>
2023-06-09 16:32:24 +0900

    ncf add

---
+++ nfc.py
@@ -0,0 +1,13 @@
+#
+# nfc.py
+# Wireless Network Term Project
+#
+# Created by IT DICE on 2023/06/05.
+#
+import subprocess
+
+with subprocess.Popen(["nfc-poll"], stdout=subprocess.PIPE) as process:
+    nfc_raw_data: str = process.stdout.read().decode("utf-8")
+    nfc_uid = nfc_raw_data[nfc_raw_data.find('UID')+14:nfc_raw_data.find('UID')+27]
+
+print(f"Result => {nfc_uid}")
