e90bd7a09276c682cc99d847d32fe6458bde74c7
itdice <itdiceofficial@gmail.com>
2023-06-09 17:11:08 +0900

    OLED 추가

---
+++ oled.py
@@ -0,0 +1,31 @@
+#
+# oled.py
+# Wireless Network Term Project
+#
+# Created by IT DICE on 2023/06/05.
+#
+import time
+import Adafruit_SSD1306
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+display = Adafruit_SSD1306.SSD1406_128_64(rst=24)
+
+display.begin()
+width = display.width
+height = display.height
+
+display.clear()
+display.display()
+
+font = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 15)
+image = Image.new('1',(width, height))
+draw = ImageDraw.Draw(image)
+
+draw.text((100, 100), 'TEST OLED', font=font, fill=255)
+
+display.image(image)
+display.display()
+time.sleep(3)
+
