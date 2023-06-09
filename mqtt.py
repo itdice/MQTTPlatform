529c445e150e46e02fb49e2197a39328f15fb789
itdice <itdiceofficial@gmail.com>
2023-06-09 23:28:00 +0900

    mqtt 오류 해결 3

--- mqtt.py
+++ mqtt.py
@@ -7,5 +7,5 @@
 import paho.mqtt.client as mqtt
 
 mqtt_client = mqtt.Client("raspi_pub")
-mqtt_client.connect("localhost", 1833)
+mqtt_client.connect("localhost", 1883)
 mqtt_client.publish("test/test", "hello!")
