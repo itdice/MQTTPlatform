3ee56ca9f28919df6c7fe0e6512b328dfef1e8e9
itdice <itdiceofficial@gmail.com>
2023-06-09 18:38:12 +0900

    fan 추가

---
+++ fan.py
@@ -0,0 +1,33 @@
+#
+# fan.py
+# Wireless Network Term Project
+#
+# Created by IT DICE on 2023/06/05.
+#
+import RPi.GPIO as GPIO
+import time
+
+INA = 9
+INB = 10
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(INA, GPIO.output)
+GPIO.setup(INB, GPIO.output)
+GPIO.output(INA, False)
+GPIO.output(INB, False)
+
+try:
+    while True:
+        GPIO.output(INA, True)
+        time.sleep(2.0)
+        GPIO.output(INA, False)
+        time.sleep(0.5)
+
+        GPIO.output(INB, True)
+        time.sleep(2.0)
+        GPIO.output(INB, False)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.output(INA, False)
+    GPIO.output(INB, False)
+    GPIO.cleanup()
--- rgb.py
+++ rgb.py
@@ -1,5 +1,5 @@
 #
-# nfc.py
+# rgb.py
 # Wireless Network Term Project
 #
 # Created by IT DICE on 2023/06/05.
