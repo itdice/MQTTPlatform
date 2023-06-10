5b14096dfc424c737b5acc06288e7b5531028bb6
itdice <itdiceofficial@gmail.com>
2023-06-10 13:56:24 +0900

    11차 테스트

--- iotplatform.py
+++ iotplatform.py
@@ -90,16 +90,13 @@ def ultrasonic_read(_range: float) -> bool:
     ECHO = 24
 
     GPIO.output(TRIG, True)  # Trigger Pulse 10us
-    print("Triggered")
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
 
     while GPIO.input(ECHO) == 0:
-        print("echo start")
         start = time.time()
 
     while GPIO.input(ECHO) == 1:
-        print("echo end")
         stop = time.time()
 
     check_time: float = stop - start
